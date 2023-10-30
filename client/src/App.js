70bb0bfc3b35c9b8ca0d83045485a3ec2bc77705
CinemaAddict <brokenswords21@gmail.com>
2023-10-30 11:01:55 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
                     )
                   }
                 />
-                <Route path="profile" />
+                <Route path="profile">
                   <Route path=":userId" element={<ProfilePage />} />
                 </Route>
                 <Route
